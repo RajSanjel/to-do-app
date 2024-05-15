759bc139d3194b1ed05101f737ba9414a8d4671a
Raj Prasad Sanjel <rajsanjel201@gmail.com>
2024-05-15 16:33:41 +0545

    UI completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { AddToDo } from "./components/AddToDo";
+import { DisplayToDo } from "./components/DisplayToDo";
+
 function App() {
   return (
-    <>
+    <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
       <h1 style={{ textAlign: "center" }}>To Do List</h1>
-
       <AddToDo />
       <DisplayToDo />
-    </>
+    </div>
   );
 }
 
